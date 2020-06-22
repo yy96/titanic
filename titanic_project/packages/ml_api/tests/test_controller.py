9a9a2cfd2eaabd650a62adcbf7e9e23f7b9f805e
yy96 <chenyy1996@yahoo.com>
2020-06-22 22:09:43 +0800

    add config and logging

---
+++ titanic_project/packages/ml_api/tests/test_controller.py
@@ -0,0 +1,7 @@
+# pass in the fixture value
+def test_health_endpoint_returns_200(flask_test_client):
+    # When
+    response = flask_test_client.get('/health')
+
+    # Then
+    assert response.status_code == 200
